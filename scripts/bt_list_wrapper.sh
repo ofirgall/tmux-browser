7f0f80f5c8b1cffbf38d32e42ccec467e1964dea
Vilhelm <52938064+h170919@users.noreply.github.com>
2025-07-15 13:43:02 +0200

    #9 reapplied without accidental unrelated formatting change

--- scripts/bt_list_wrapper.sh
+++ scripts/bt_list_wrapper.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$CURRENT_DIR/helpers.sh"
 
 TIMEOUT="$(tmux_option "@browser_brotab_timeout" "5.0")"
 bt_list=$(timeout $TIMEOUT bt list)
 
 if [ $? != 0 ]; then
-  # TODO: to avoid this we need to keepalive the mediator from the extension side
-
-  log_error \
-    "[ERROR] Multiple windows with same tab id: $tab_id_name."
-  exit 1
+	# TODO: to avoid this we need to keepalive the mediator from the extension side
+	log_error \
+		"[ERROR] Multiple windows with same tab id: $tab_id_name."
+	exit 1
 fi
 
 echo "$bt_list"
